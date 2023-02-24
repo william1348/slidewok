e283f1dbef5072053386069ac6260e293a01b7fd
will <wkung42@gmail.com>
2023-02-24 11:51:48 -0700

    commit

--- public/js/projects.js
+++ public/js/projects.js
@@ -15,9 +15,24 @@ function populateSetlist(data){
   notes = data.notes;
   count = 0;
 
+  // metadata
+  $('#setlist-title').append(data.title);
   $('#setlist-date-long').append(data.dateLong);
-  $('#setlist-location').append(data.location);
+  $('#setlist-location').append(", " + data.location);
   $('#setlist-notes').append(notes);
+
+  // audio 
+  if(data.nugsLink != null){
+    $('#nugs-link').append("<a href='" + "http://www.nugs.com" + "'>NUGS");
+  }else{
+    $('#nugs-link').append("<a href='" + "http://www.nugs.com" + "'>NUGS");
+  }
+
+  if(data.archiveLink != null){
+    $('#archive-link').append("<a href='" + "http://www.archive.org" + "'>ARCHIVE");
+  }else{
+    $('#archive-link').append("<a href='" + "http://www.archive.org" + "'>ARCHIVE");
+  }
 
 
   console.log('number of sets ' + setlist.length);
@@ -27,29 +42,29 @@ function populateSetlist(data){
     console.log('number of items in set ' + setArray.length);
     for(var j = 0; j<setArray.length; j++){
       setObj = setArray[j];
-      if(setObj["type"] == "identifier"){
-        console.log('identifier : ' + setObj["name"]);
-        $setlistContainer.append(setObj["name"] + ": ");
-      }else if (!setObj["type"]){
-        //var link = $('<a>', {href: "stats.html/" + setObj["song"]["id"]});
-        // console.log("song " + setObj["song"]["name"]);
-        // $setlistContainer.append(link);
-        $setlistContainer.append("<span class='song-link' song-id ='" + setObj["song"]["id"] + "'>" +  setObj["song"]["name"] + "</div>");
-      } else{
-        if(setObj["type"] == ">"){
-          $setlistContainer.append(" " + setObj["type"] + " ");
+      if (!setObj["identifier"]){ // song link
+       // console.log('song: ' + setObj["song"]["name"]);
+        if(setObj["song"]["note"] != null){ 
+          $setlistContainer.append("<span class='song-link' song-id ='" + setObj["song"]["id"] + "'>" +  setObj["song"]["name"] + "<sup>" + setObj["song"]["note"] + "</sup></div>");
+        }else{
+          $setlistContainer.append("<span class='song-link' song-id ='" + setObj["song"]["id"] + "'>" +  setObj["song"]["name"] + "</div>");
         }
-        if(setObj["type"] == ","){
-          $setlistContainer.append(setObj["type"] + " ");
+      } else{
+        if(setObj["identifier"].includes("Set")){
+          $setlistContainer.append(setObj["identifier"] + ": ");
+        }else if(setObj["identifier"] == ">"){
+          $setlistContainer.append("<span class='song-separator'> " + setObj["identifier"] + " </span>");
+        } else if(setObj["identifier"] == ","){
+          $setlistContainer.append("<span class='song-separator'>" + setObj["identifier"] + " </span>");
         }
       }
     }
-    $setlistContainer.append("<br><br>");
+    $setlistContainer.append("<br>");
   }
 
   $('.song-link').click(function(){
     songId = $(this).attr("song-id");
-    $('#song-title').text($(this).text());
+    $('#song-title').text($(this).html());
 
     $.ajax({
         url: "/songs/" + songId
@@ -62,10 +77,24 @@ function populateSetlist(data){
 
 function populateSong(song){
   if(song == null){
-    $("#song-container").hide();
+    $("#song-metadata-container").hide();
     $("#no-song-info").show();
   }else{
+    $('#song-title').text(song.name);
+    $("#song-metadata-container").show();
     $("#no-song-info").hide();
-    $("#song-container").show();
+
+    $("#song-info-author").text(song.author);
+    $("#song-info-ltp").text(song.ltp);
+    $("#song-info-play-count").text(song.playCount);
+
+    $("#song-info-ltp").text(song.ltp);
+
+    $("#song-info-average-gap").text("6 shows");
   }
+
+  // function calculateAverageGap(count, start){
+   
+  // }
+
 }
